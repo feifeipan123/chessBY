e764b45ff0655460ae7390f53f1be2ce80e8b0ac
feifiepan <405041418@qq.com>
2018-05-03 12:04:07 +0800

    增加走棋规则

--- board.cpp
+++ board.cpp
@@ -2,6 +2,9 @@
 #include <QPainter>
 #include <QMouseEvent>
 #include <QDebug>
+#define GetRowCol(__row, __col, __id) \
+    int __row = _s[__id]._row;\
+    int __col = _s[__id]._col
 
 Board::Board(QWidget *parent) : QWidget(parent),_selectid(-1)
 {
@@ -53,6 +56,12 @@ QPoint Board::center(int id){
     return center(_s[id]._row,_s[id]._col);
 }
 
+bool Board::isDead(int id)
+{
+    if(id == -1)return true;
+    return _s[id]._dead;
+}
+
 void Board::drawStone(QPainter& painter,int id){
     if(_s[id]._dead)
         return;
@@ -84,11 +93,126 @@ bool Board::getRowCol(QPoint pt,int &row,int &col){
             if(dist<_r*_r){
                 return true;
             }
-
         }
     }
     return false;
+}
+//根据行列值获取棋子id
+int Board::getStoneId(int row, int col)
+{
+    for(int i=0; i<32; ++i)
+    {
+        if(_s[i]._row == row && _s[i]._col == col && !isDead(i))
+            return i;
+    }
+    return -1;
+}
 
+int Board::relation(int row1, int col1, int row, int col)
+{
+    return qAbs(row1-row)*10+qAbs(col1-col);
+}
+
+//将的走棋规则
+bool Board::canMoveJiang(int moveid,int row,int col,int killid){
+    /*
+     * 1.位置在九宫格之内
+     * 2.移动步长是一个格子
+     */
+    GetRowCol(row1, col1, moveid);
+    int r = relation(row1, col1, row, col);
+    //只能走一格
+    if(r != 1 && r != 10){
+        return false;
+    }
+    //列不分红黑棋子
+    if(col < 3 || col > 5) return false;
+    //行分红黑
+    if(_s[moveid]._red){
+        if(row>2)return false;
+    }else{
+        if(row<7)return false;
+    }
+    return true;
+}
+
+//士的走棋规则
+bool Board::canMoveShi(int moveid,int row,int col,int killid){
+    /*
+     * 1.位置在九宫格之内
+     * 2.移动步长是一个格子
+     */
+    GetRowCol(row1, col1, moveid);
+    int r = relation(row1, col1, row, col);
+    if(r != 11) return false;
+
+    if(col < 3 || col > 5) return false;
+    if(_s[moveid]._red){
+        if(row>2)return false;
+    }else{
+        if(row<7)return false;
+    }
+    return true;
+}
+
+bool Board::canMoveXiang(int moveid,int row,int col,int killid){
+
+    GetRowCol(row1, col1, moveid);
+    int r = relation(row1, col1, row, col);
+    if(r != 22) return false;
+
+    int rEye = (row+row1)/2;
+    int cEye = (col+col1)/2;
+    if(getStoneId(rEye, cEye) != -1)//象眼位置不能有棋子
+        return false;
+    if(_s[moveid]._red)
+    {
+        if(row > 5) return false;
+    }
+    else
+    {
+        if(row < 4) return false;
+    }
+    return true;
+}
+
+bool Board::canMoveMa(int moveid,int row,int col,int)
+{
+    GetRowCol(row1, col1, moveid);
+    int r = relation(row1, col1, row, col);
+    if(r != 12 && r != 21)
+        return false;
+
+    if(r == 12)
+    {
+        if(getStoneId(row1, (col+col1)/2) != -1)
+            return false;
+    }
+    else
+    {
+        if(getStoneId((row+row1)/2, col1) != -1)
+            return false;
+    }
+    return true;
+}
+
+bool Board::canMoveBing(int moveid, int row, int col, int)
+{
+    GetRowCol(row1, col1, moveid);
+    int r = relation(row1, col1, row, col);
+    if(r != 1 && r != 10) return false;
+
+    if(_s[moveid]._red)
+    {
+        if(row < row1) return false;
+        if(row1 <= 4 && row == row1) return false;
+    }
+    else
+    {
+        if(row > row1) return false;
+        if(row1 >= 5 && row == row1) return false;
+    }
+    return true;
 }
 
 bool Board::canMove(int moveid,int row,int col,int killid){
@@ -99,7 +223,27 @@ bool Board::canMove(int moveid,int row,int col,int killid){
         update();
         return false;
     }
-    return true;
+    switch(_s[moveid]._type){
+        case Stone::JIANG:
+            return canMoveJiang(moveid,row,col,killid);
+            break;
+        case Stone::SHI:
+            return canMoveShi(moveid,row,col,killid);
+            break;
+        case Stone::XIANG:
+            return canMoveXiang(moveid,row,col,killid);
+            break;
+        case Stone::CHE:
+            break;
+        case Stone::MA:
+            return canMoveMa(moveid,row,col,killid);
+            break;
+        case Stone::PAO:
+            break;
+        case Stone::BING:
+            return canMoveBing(moveid,row,col,killid);
+            break;
+    }
 }
 
 void Board::mouseReleaseEvent(QMouseEvent *ev){
@@ -117,10 +261,12 @@ void Board::mouseReleaseEvent(QMouseEvent *ev){
     if(i<32){
         clickid=i;
     }
-    if(_selectid==-1){//选中棋子
-        if(clickid!=-1){
-            _selectid = clickid;
-             update();
+    if(_selectid==-1){//可以选择棋子了
+        if(clickid!=-1){//能选中棋子
+            if(bRedTurn==_s[clickid]._red){//是红棋走
+                _selectid = clickid;
+                 update();
+            }
         }
     }else{//走棋
         if(canMove(_selectid,row,col,clickid)){
@@ -130,8 +276,8 @@ void Board::mouseReleaseEvent(QMouseEvent *ev){
                 _s[clickid]._dead=true;
             }
             _selectid = -1;
+            bRedTurn = !bRedTurn;
             update();
         }
     }
-
 }
--- board.h
+++ board.h
@@ -12,13 +12,22 @@ public:
     Stone _s[32];
     int _r;//棋子的半径
     int _selectid;//选中的棋子标号
+    bool bRedTurn = true;//轮到红棋走
     /*返回象棋棋盘行列对应的像素坐标*/
     QPoint center(int row,int col);
     QPoint center(int id);
     void drawStone(QPainter& painter,int id);
+    int relation(int row1, int col1, int row, int col);
+    int getStoneId(int row, int col);
+    bool isDead(int id);
     //获取鼠标点击点的行列值
     bool getRowCol(QPoint pt,int &row,int &col);
     bool canMove(int moveid,int row,int col,int killid);
+    bool canMoveJiang(int moveid,int row,int col,int killid);
+    bool canMoveShi(int moveid,int row,int col,int killid);
+    bool canMoveXiang(int moveid,int row,int col,int killid);
+    bool canMoveMa(int moveid, int row, int col, int);
+    bool canMoveBing(int moveid, int row, int col, int);
 
     void paintEvent(QPaintEvent *);
     void mouseReleaseEvent(QMouseEvent *);
