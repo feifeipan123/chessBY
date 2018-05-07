537d3d2d59cdb3f50ee2747c0d07555cebfe0447
feifiepan <405041418@qq.com>
2018-05-07 08:52:15 +0800

    增强人工智能

--- singlegame.h
+++ singlegame.h
@@ -4,14 +4,21 @@
 
 class SingleGame:public Board
 {
+    Q_OBJECT
 public:
+    SingleGame();
     virtual void click(int id, int row, int col);
     void getAllPossibleMove(QVector<Step *> &steps);
     Step* getBestMove();
     void fakeMove(Step *step);
     void unfakeMove(Step* step);
     int calcScore();
-    int getMinScore();
+    int getMinScore(int level,int curMaxScore);
+    int getMaxScore(int level,int curMinScore);
+    int _level;
+
+public slots:
+    void computerMove();
 };
 
 #endif // SINGLEGAME_H
